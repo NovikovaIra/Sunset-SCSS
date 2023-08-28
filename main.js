8568f6b7c649d3ff9170e61c6a29994a0ea90aed
Novikova Ira <novikova.mrr@gmail.com>
2023-08-28 19:31:53 +0300

    Made a slider

--- main.js
+++ main.js
@@ -1,2 +1,42 @@
 import './node_modules/reset-css/sass/_reset.scss';
 import './main.scss';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop:true,
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+      768: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+      },
+      1024: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+      },
+      1280: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+      }
+  }
+});
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
+
+// import Swiper from 'swiper/bundle';
+
+// // import styles bundle
+// import 'swiper/css/bundle';
+
+// // init Swiper:
+// const swiper = new Swiper(...);
